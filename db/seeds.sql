9ab0e990d9529b0222b782e37ba62f010adea1e1
SeanB333 <spb4578@gmail.com>
2018-11-23 12:53:55 -0600

    init files

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Hamburger');
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Baconator', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Tower', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Salmon Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Megatron Burger');
